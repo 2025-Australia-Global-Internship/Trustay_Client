79222f98fdbc34667268e5c6b2c5571e418fb3e6
Mingarden08 <jeongone.min@gmail.com>
2026-04-10 13:04:21 +0900

    payment api

--- back/trustay/src/main/java/com/maritel/trustay/repository/MemberRepository.java
+++ back/trustay/src/main/java/com/maritel/trustay/repository/MemberRepository.java
@@ -1,7 +1,10 @@
 package com.maritel.trustay.repository;
 
 import com.maritel.trustay.entity.Member;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -13,6 +16,7 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 
     Optional<Member> findByEmail(String email);
 
-
-
+    @EntityGraph(attributePaths = "profile")
+    @Query("SELECT m FROM Member m WHERE m.id = :id")
+    Optional<Member> findWithProfileById(@Param("id") Long id);
 }
