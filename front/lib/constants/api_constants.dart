92fad67519c9786c0b162dffd324cd4821faa848
윤성연 <s2427@e-mirim.hs.kr>
2026-05-26 18:37:14 +0900

    재설정

--- front/lib/constants/api_constants.dart
+++ front/lib/constants/api_constants.dart
@@ -1,6 +1,7 @@
 class ApiConstants {
   // 기본 도메인(끝에 슬래시 없음)
-  static const String baseUrl = 'http://10.0.2.2:8080'; //    -- 로컬
+  // static const String baseUrl = 'http://10.0.2.2:8080'; //    -- 로컬
   // static const String baseUrl = 'https://trustay.digitalbasis.com'; // -- 서버
-
+  static const String baseUrl =
+      'http://trustay.mirim-it-show.site:8080'; // -- 서버
 }
